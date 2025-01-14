4e2d22fca144785d369e94dcb9c8a254adda7a27
irened123 <irenedeng7@gmail.com>
2025-01-13 22:41:22 -0500

    Add AdminReviewAction model

---
+++ src/main/java/com/irened/diningreviewapi/model/AdminReviewAction.java
@@ -0,0 +1,12 @@
+package com.irened.diningreviewapi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class AdminReviewAction {
+    private Boolean accepted;
+}
