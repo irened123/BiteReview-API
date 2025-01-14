0ec56fa86f9d07b9128b3b379c694f9eb24ec917
irened123 <irenedeng7@gmail.com>
2025-01-13 22:04:44 -0500

    Add User entity

--- src/main/java/com/irened/diningreviewapi/model/User.java
+++ src/main/java/com/irened/diningreviewapi/model/User.java
@@ -4,25 +4,27 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "users") 
 @Getter
 @Setter
 @RequiredArgsConstructor
-public class Restaurant {
-
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-    private String location;
+    private String displayName; 
+    private String city;
+    private String state;
+    private String zipCode;
 
-    private Double peanutScore;
-    private Double eggScore;
-    private Double dairyScore;
-    private Double overallScore;
+    private Boolean interestedInPeanutAllergies;
+    private Boolean interestedInEggAllergies;
+    private Boolean interestedInDairyAllergies;
 }
